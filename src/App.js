5bdb83177dd2200310d8238a4ffc039dd9157d8a
shu8091 <shu8091@gmail.com>
2021-03-06 03:02:47 +0800

    presentation components FormInput and CustomButton, finish sign in form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route exact path='/shop' component={ ShopPage } />
+        <Route exact path='/signin' component={ SignInAndSignUp } />
       </Switch>
     </div>
   );
